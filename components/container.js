efad31ceeab2f449252c7fc083e73f3729ccad6e
alinsalaru <alin.salaru@gmail.com>
2016-06-03 23:51:35 +0300

    added delete movie functionality

--- components/container.js
+++ components/container.js
@@ -2,6 +2,19 @@ import React from 'react';
 import MovieRow from './movierow';
 
 export default React.createClass({
+	deleteMovie : function(id){
+		var movies = this.props.movies;
+		var index;
+		for (var i = 0; i < movies.length; i++) {
+			if(movies[i].id === id) {
+				index = i;
+			}
+		}
+		if (index > -1) {
+		    movies.splice(index, 1);
+		}
+		console.log(movies);
+	},
 	render: function() {
 		var movies= this.props.movies;
 		console.log(movies);
