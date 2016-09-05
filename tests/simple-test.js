ace7b4a978dd1417b4491c0996da6949abfdf80c
alinsalaru <alin.salaru@gmail.com>
2016-09-05 13:23:59 +0300

    added tests for reducers

--- tests/simple-test.js
+++ tests/simple-test.js
@@ -1,7 +1,23 @@
 import expect from 'expect';
 import deepFreeze from 'deep-freeze';
 
-describe("A simple test", function() {
+//movieList
+var movieList = [
+	{name: 'Batman'},
+	{name: 'Pulp Fiction'},
+	{name: 'Donnie Darko'}
+];
+
+function addMovieToMovieList(nameGiven, movieList) {
+    return movieList.concat([{name : nameGiven}]);
+}
+
+function removeMovieFromMovieList(index,movieList) {
+    return movieList.slice(0,index)
+           .concat(movieList.slice(index+1));
+}
+
+describe("A simple test: ", function() {
     it("checks to see if null doesn't exists", function() {
         expect(null).toNotExist();
     });
@@ -15,4 +31,17 @@ describe("A simple test", function() {
        expect(tryFreeze).toThrow(/extensible/g);
        
     });
+
+    it("should add new movie", function(){
+        deepFreeze(movieList);
+        expect( addMovieToMovieList("Batman Returns", movieList)).toInclude(
+            {name: 'Batman Returns'}
+        );
+    });
+    it("should remove an existing movie", function(){
+        expect(removeMovieFromMovieList(0,movieList)).toExclude(
+            {name: 'Batman'}
+        );
+    });
+
 });
