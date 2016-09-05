759b52e885ed7d52c1e6fe76706bfd2c8ea9c38d
alinsalaru <alin.salaru@gmail.com>
2016-09-05 14:16:05 +0300

    added tests for Movie Reducer

--- tests/simple-test.js
+++ tests/simple-test.js
@@ -3,19 +3,80 @@ import deepFreeze from 'deep-freeze';
 
 //movieList
 var movieList = [
-	{name: 'Batman'},
-	{name: 'Pulp Fiction'},
-	{name: 'Donnie Darko'}
+	{name: 'Batman' , id: 0},
+	{name: 'Pulp Fiction', id : 1},
+	{name: 'Donnie Darko', id : 2}
 ];
 
-function addMovieToMovieList(nameGiven, movieList) {
-    return movieList.concat([{name : nameGiven}]);
+const movies = (state = [], action ) => {
+    switch(action.type) {
+        case "ADD_MOVIE" : {
+            return state.concat([{
+                name : action.name,
+                id : action.id
+            }]);
+        }
+        case "IMPORT_LIST" : {
+            return state.concat(action.listToImport)
+        }
+        case "REMOVE_MOVIE" : {
+            return state.slice(0,action.id)
+           .concat(state.slice(action.id+1));
+        }
+    }
+};
+
+const testImportList = () => {
+    const stateBefore =[];
+    const action = {
+        type : "IMPORT_LIST",
+        listToImport : movieList
+    }
+    const stateAfter = stateBefore.concat(movieList);
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(movies(stateBefore,action)).toEqual(stateAfter);
+    console.log(stateAfter, 'praaa');
 }
 
-function removeMovieFromMovieList(index,movieList) {
-    return movieList.slice(0,index)
-           .concat(movieList.slice(index+1));
+
+const testAddMovie = () => {
+    const stateBefore = [];
+    const action = {
+        type : "ADD_MOVIE",
+        id : 0,
+        name : 'Batman' 
+    }
+    const stateAfter = [
+        {
+            id : 0,
+            name : 'Batman'
+        }
+    ]
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(movies(stateBefore,action)).toEqual(stateAfter);
+
 }
+
+const testRemoveMovie = () => {
+    const stateBefore = movieList;
+    const action = {
+        type : "REMOVE_MOVIE",
+        id : 0 
+    }
+    const stateAfter = [
+        {name: 'Pulp Fiction', id : 1},
+        {name: 'Donnie Darko', id : 2}
+    ]
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(movies(stateBefore,action)).toEqual(stateAfter);
+}
+
 
 describe("A simple test: ", function() {
     it("checks to see if null doesn't exists", function() {
@@ -32,16 +93,15 @@ describe("A simple test: ", function() {
        
     });
 
-    it("should add new movie", function(){
-        deepFreeze(movieList);
-        expect( addMovieToMovieList("Batman Returns", movieList)).toInclude(
-            {name: 'Batman Returns'}
-        );
-    });
-    it("should remove an existing movie", function(){
-        expect(removeMovieFromMovieList(0,movieList)).toExclude(
-            {name: 'Batman'}
-        );
+    it("should add new movie",function() {
+        testAddMovie();
     });
 
+    it("should import list of movies", function(){
+        testImportList();
+    });
+
+    it("should remove movie by id",function() {
+        testRemoveMovie();
+    });
 });
